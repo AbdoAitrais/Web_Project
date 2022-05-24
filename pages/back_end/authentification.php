6f14ff807e171bdeb331b446f89c7a26064567c0
abdo <abdo-pro@hotmail.com>
2022-05-24 15:52:15 +0100

    connexion php separated from auth

--- pages/back_end/authentification.php
+++ pages/back_end/authentification.php
@@ -1,24 +1,7 @@
 
 <?php  
 
-		ob_start();
-		session_start();
-
-		$server = "localhost";//specifier le port si vous utiliser plusieurs serveurs
-		$dataB = "bd_fstage";
-		$dsn = "mysql:host=$server;dbname=$dataB";
-		$user = "root";
-		$password = "";
-
-		try
-		{
-			$bdd = new PDO($dsn,$user,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-			echo "test0";
-		}
-		catch (PDOException $e)
-		{
-			die('Erreur : '. $e->getMessage());
-		}
+		include('connexion.php');
 	
 
 ?>
