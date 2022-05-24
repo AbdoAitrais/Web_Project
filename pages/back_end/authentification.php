fa9ba9e1df7b4a183299be55333af513a85f9463
abdo <abdo-pro@hotmail.com>
2022-05-24 15:47:52 +0100

    Login back end done 3 types

--- pages/back_end/authentification.php
+++ pages/back_end/authentification.php
@@ -1,11 +1,10 @@
 
 <?php  
-	function()
-	{
+
 		ob_start();
 		session_start();
 
-		$server = "localhost:3306";//specifier le port si vous utiliser plusieurs serveurs
+		$server = "localhost";//specifier le port si vous utiliser plusieurs serveurs
 		$dataB = "bd_fstage";
 		$dsn = "mysql:host=$server;dbname=$dataB";
 		$user = "root";
@@ -14,45 +13,66 @@
 		try
 		{
 			$bdd = new PDO($dsn,$user,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			echo "test0";
 		}
 		catch (PDOException $e)
 		{
 			die('Erreur : '. $e->getMessage());
 		}
-	}
+	
+
 ?>
 
 <?php
-if( !empty($_POST['username']) && !empty($_POST['password']) )
-{
-	$username = htmlspecialchars($_POST['username']);
-	$password = htmlspecialchars($_POST['password']);
 
-	$Smt = $bdd->prepare('SELECT * FROM etudiant WHERE LOGIN_ETU=? and PASS_ETU=?');
-	$Smt -> execute(array($username,$password));
-	$rows = $Smt -> fetch();
-	$Smt->closeCursor();//vider le curseur (free)
-	
-	if( !empty($rows) )
+	echo "test1";
+	if( !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['type_user']) )
 	{
-		$_SESSION['pseudo'] = $username;
-		if(isset($_SESSION['error']))
+		echo "test2";
+		$username = htmlspecialchars($_POST['username']);
+		$password = htmlspecialchars($_POST['password']);
+		$type_user = $_POST['type_user'];
+		
+		if ($type_user === "Etudiant") 
 		{
-			unset($_SESSION['error']);
+			$Smt = $bdd->prepare('SELECT * FROM etudiant WHERE LOGIN_ETU=? and PASS_ETU=?');
+			$MAINPAGE = '../zind.html';
+		}
+		else if ($type_user === "Responsable")
+		{
+			$Smt = $bdd->prepare('SELECT * FROM formation WHERE LOGIN_RESP=? and PASS_RESP=?');
+			$MAINPAGE = '../responsable.html';
+		}
+		else if ($type_user === "Admin")
+		{
+			$Smt = $bdd->prepare('SELECT * FROM admin WHERE LOGIN_ADMIN=? and PASS_ADMIN=?');
+			$MAINPAGE = '../admin.html';
+		}
+		
+		$Smt -> execute(array($username,$password));
+		$rows = $Smt -> fetch();
+		$Smt->closeCursor();//vider le curseur (free)
+		
+		if( !empty($rows) )
+		{
+			echo "test3";
+			$_SESSION['pseudo'] = $username;
+			
+			if(isset($_SESSION['error']))
+			{
+				unset($_SESSION['error']);
+			}
+
+			if(!isset($_SESSION['page']))
+				header('location:'.$MAINPAGE);
+			else
+				header('location:'.$_SESSION['page']);
+				
 		}
 		else
 		{
-			if(!isset($_SESSION['page']))
-				header('location: zind.html');
-			else
-				header('location:'.$_SESSION['page']);
-			
+			$_SESSION['error'] = 'Incorrect login !';
+			header('location: ../login.html');
 		}
 	}
-	else
-	{
-		$_SESSION['error'] = 'Incorrect login !';
-		header('location: login.html');
-	}
-}
 ?>
---
+++ pages/back_end/logout.php
@@ -0,0 +1,8 @@
+<?php 
+ob_start();
+session_start();
+$_SESSION['pseudo'] = '';
+session_destroy();
+//echo $_SESSION['pseudo'];
+header('location: ../login.html');
+?>
