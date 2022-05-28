c02e562265bbc8da758b83dfcc22e6a95688a65f
Yassine <yassinejrayfy36@gmail.com>
2022-05-28 17:13:19 +0100

    hh chi haja added ahhh navbar hh

--- pages/Modifier_Etudiant_Resp.php
+++ pages/Modifier_Etudiant_Resp.php
@@ -73,7 +73,7 @@
                 <a class="nav-link navlink " href="Find_Offre_Resp.php">Find offers</a>
               </li>
               <li class="nav-item underline">
-                <a class="nav-link navlink" href="#">Historique</a>
+                <a class="nav-link navlink" href="Historique.php">Historique</a>
               </li>
               <li class="nav-item underline">
                 <a class="nav-link navlink active_link_color" href="ListeEtudiants.php">Etudiants</a><span class="active_link_line"></span>
@@ -201,6 +201,10 @@
 
           </div>
         </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+        crossorigin="anonymous"></script>
           
     
 </body>
