79df25095832912a5ae9a259edc5ccdbe927ffdc
abdo <abdo-pro@hotmail.com>
2022-06-02 23:29:39 +0100

    cleaning code

--- pages/Modifier_Etudiant_Resp.php
+++ pages/Modifier_Etudiant_Resp.php
@@ -24,53 +24,53 @@
       if( !empty($_POST['nom_etu']) )
       { echo "<br><br><br><br>kakakaka";
         $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["cv"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$cv = NULL;
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["cv"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
+        $target_file = $target_dir . basename($_FILES["cv"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $cv = NULL;
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+          $check = getimagesize($_FILES["cv"]["tmp_name"]);
+          if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+          } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+          }
+        }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
+        // Check if file already exists
+        if (file_exists($target_file)) {
+          echo "Sorry, file already exists.";
+          $uploadOk = 0;
+        }
 
-// Check file size
-if ($_FILES["cv"]["size"] > 10000000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
+        // Check file size
+        if ($_FILES["cv"]["size"] > 10000000) {
+          echo "Sorry, your file is too large.";
+          $uploadOk = 0;
+        }
 
-// Allow certain file formats
-if($imageFileType != "pdf" && $imageFileType != "docx" && $imageFileType != "dotx"
-&& $imageFileType != "doc" ) {
-  echo "Sorry, only PDF, DOCX, DOC & DOTX files are allowed.";
-  $uploadOk = 0;
-}
+        // Allow certain file formats
+        if($imageFileType != "pdf" && $imageFileType != "docx" && $imageFileType != "dotx"
+        && $imageFileType != "doc" ) {
+          echo "Sorry, only PDF, DOCX, DOC & DOTX files are allowed.";
+          $uploadOk = 0;
+        }
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["cv"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["cv"]["name"])). " has been uploaded.";
-    $cv = "uploads/".htmlspecialchars( basename( $_FILES["cv"]["name"]));
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+          echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+          if (move_uploaded_file($_FILES["cv"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["cv"]["name"])). " has been uploaded.";
+            $cv = "uploads/".htmlspecialchars( basename( $_FILES["cv"]["name"]));
+          } else {
+            echo "Sorry, there was an error uploading your file.";
+          }
+        }
        
         $id_etu = htmlspecialchars($_POST['id_etu']);
         $nom_etu = htmlspecialchars($_POST['nom_etu']);
