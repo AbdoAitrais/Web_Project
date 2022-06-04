6bb468173c8991a0d78a47534e6349be41bf8cd5
abdo <abdo-pro@hotmail.com>
2022-06-04 21:35:40 +0100

    FindOfferResp version 2

--- pages/Find_Offre_Resp.php
+++ pages/Find_Offre_Resp.php
@@ -9,6 +9,17 @@
 
   if($_SESSION['user_type'] == "Responsable")
   {
+
+    require("back_end/connexion.php");
+
+    $id_form = $_SESSION['user_id'];
+                                
+    ///Tous les offres de cette formation
+    $Smt = $bdd->prepare("SELECT * FROM offre O,entreprise E WHERE E.ID_ENTREP=O.ID_ENTREP AND O.ID_FORM=?");
+    $Smt -> execute(array($id_form));
+	  $rows = $Smt -> fetchAll(PDO::FETCH_ASSOC);
+    $Smt->closeCursor();
+
 ?>
 
 <!DOCTYPE html>
@@ -17,20 +28,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Offre</title>
-    <link rel="stylesheet" href="pub_etud.css">
+    <link rel="stylesheet" href="nextab.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
     rel="stylesheet" 
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
     crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+	<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <title>Find Offers</title>
+    
+    
 </head>
 <body>
-
-  
-    
-    <nav class="navbar navbar-expand-lg navbar-light bg-light position-fixed" style="z-index: 9; width: 100%; top: 0;background: #F3F5F8 !important;">
+      
+<nav class="navbar navbar-expand-lg navbar-light bg-light position-fixed" style="z-index: 9; width: 100%; top: 0;background: #F3F5F8 !important;">
         <div class="container-fluid">
           <a class="navbar-brand navt d-lg-block d-lg-none" href="#">FSTAGE</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -72,169 +86,154 @@
           </div>
         </div>
       </nav>
-    
 
     <div class="container-fluid ">
-      <div class="" style="margin-top: 56px;">
-        <div class="row">
-          <div class="col-3 d-none d-md-block elm guid1_col" ></div>
-          <form action="Find_Offre_Resp.php" method='POST'>
-            <div class="col-md-6 col-sm-12 elm pub_col" style="position:fixed; text-align: center; display:flex; justify-content:center;z-index:9 !important;">
-              <div class="search">
-                  <div class="input-group rounded">
-                      <input type="search" class="form-control rounded" name='Filter' placeholder="Type a Keyword, Title, City" aria-label="Search" aria-describedby="search-addon" />
-                      <span class="input-group-text border-0" id="search-addon">
-                          <button type='submit' style="border:none;background:none;"><i class="fas fa-search"><img src="icons/search.png"></i></button>
-                      </span>
-                  </div>
-                </div>
-              </div>
-          </form>
-          <div class="col-3 d-none d-md-block elm blank_col" ></div>
-        </div>
-
-        <div class="row" style="margin-top: 56px;">
-          <div class="col-12  d-md-none elm">
-
-            <div class="greenc" style="display:inline-block;margin-left: 0%;">
-                 <span class="guide2"> Nouveau </span>
-            </div>
-
-            <div class="grayc" style="display:inline-block; margin-left: 27%;">
-                 <span class="guide2"> Complétée </span> 
-            </div>
-
-            <div class="redc" style="display:inline-block; margin-left: 34%;">
-              <span class="guide2">Close</span>
-            </div>
-
-          </div>
-        </div>
+      <div class="" style="margin-top: 150px;">
+        
 
 
-        <div class="row">
-
-          <div class="col-3 d-none d-md-block elm guid1_col">
-            <div class="guid2_col">
-
-              <div class="greenc"> 
-                <span class="guide"> Nouveau </span>
-              </div> <br>
-
-              <div class="grayc"> 
-                <span class="guide"> Complétée </span>
-              </div> <br>
-
-              <div class="redc"> 
-                <span class="guide">Close</span>
-              </div> <br>
-
-            </div>
-          </div>
-          
-          <div class="col-md-6 col-sm-12 elm pub_col">
+        
          
-            <?php require("back_end/connexion.php");
-                    //$Etu=$_SESSION['Etu'];
-                    $Resp = $_SESSION['user_id'];
+
+
+          <div class="row" >
+            <div class="col-md-8 elm pub_col" style=" background: #FFFFFF !important;
+                        border-radius: 35px !important; padding: 5%;">
+
+                  <div class="tableHead" style="margin-bottom: 10px;">
+                        <h4>Liste des offres</h4>   
+                  </div>
+                  
+
+                <table class="table" id="Table_Offre">
+                    <thead>
+                      <tr>
+                         <th scope="col">N</th>
+                         <th scope="col">Statut</th>
+                        <th scope="col">Entreprise</th>
+                        <th scope="col">Ville</th>
+                        <th scope="col">Poste</th>
+                        <th scope="col"></th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                  
+                    <?php
+                        foreach ($rows as $row): 
+                    ?>
+                        <tr>
+                            <th scope="row" style="color: #7096FF;"><?php echo $row['NIVEAU_OFFRE']; ?></th>
+                            <?php
+                              switch ($row['STATUOFFRE']) {
+                                case 'Nouveau':
+                                  $color = '#6FF5B5';
+                                  break;
+                                case 'Completée':
+                                  $color = '#CAD2CE';
+                                  break;
+                                case 'Closed':
+                                  $color = '#E160B5';
+                                  break;
                                 
-                    ///Tous les offres de cette etudiant
-                    $sql2 ="SELECT * FROM offre O,entreprise E WHERE E.ID_ENTREP=O.ID_ENTREP AND O.ID_FORM='$Resp'";
-                    ///***Search bar
-                    if(isset($_POST['Filter']) && !empty( $_POST['Filter'] )){
-
-                      $Filter_search = $_POST['Filter'];
-                      $sql2=$sql2." AND( (E.VILLE = '$Filter_search' ) OR (O.POSTE = '$Filter_search' ) OR (O.DESCRIP LIKE '%$Filter_search%' ) OR (E.NOM_ENTREP LIKE '$Filter_search' ) )";
-
-                    }
-                    /// ***Order by
-                    $sql2=$sql2." ORDER BY O.ID_OFFRE DESC";
-                    
-                    /// ***  
-                    $req2 =$bdd->query($sql2);
-                    $result2 = $req2->fetchAll(PDO::FETCH_ASSOC);
-                    $indice = 0;
-                    if(!empty($result2))
-                    {
-                        foreach($result2 as $Offre):                  
-            ?>
-          
-            <div class="brd">
-              <?php                  
-                  if($Offre['STATUOFFRE'] == 'Nouveau')
-                        echo '<div class="greenc"> </div>'; 
-                  else if($Offre['STATUOFFRE'] == 'Completée')
-                        echo '<div class="grayc"> </div>';
-                  else if($Offre['STATUOFFRE'] == 'Closed')
-                        echo '<div class="redc"> </div>';  
-              ?>
-              
-              <div class="edit" ><i  onclick="menuToggle(<?php print($indice);?>)" ><img src="icons/edt.png" alt=""></i></div>
-              <div style="display: flex; justify-content: end; margin-right: -0px;">
-                <div class="menu" id="mn">
-            
-                  <ul>
-                    <li><img src="icons/edit.png" alt=""><a href="#">Modifier</a> </li>
-                    <li><img src="icons/file.png" alt=""><a href="#">File d'attente</a> </li>
-                  </ul>
-                </div>
-              </div><br>
-              
-              <div class="content">
-
-                <span class="poste" ><?php print($Offre['POSTE'])?></span> <br><br>
-
-                <span class="ville" ><?php print($Offre['NOM_ENTREP'])?> - <?php print($Offre['VILLE'])?></span> <br>
-
-                <span class="duree" >(Durée <?php print($Offre['DUREE']/30);?> months)</span> <br><br>
-
-                <div class="desc" >
-                  <p><?php print($Offre['DESCRIP']);?></p>
-                </div>
-
-                <div>
-                  <span class="time"> <img src="icons/time.png" alt=""> <?php print($Offre['DATEFIN']);?> </span>
-                </div>
-
+                                default:
+                                  $color = '#7096FF';
+                                  break;
+                              }
+                            ?>
+                            <td style="color: <?php echo $color; ?>;"><?php echo $row['STATUOFFRE']; ?></td>
+                          <td><?php echo $row['NOM_ENTREP']; ?></td>
+                          <td><?php echo $row['VILLE']; ?></td>
+                          <td style="color: #7096FF;"><?php echo $row['POSTE']; ?></td>
+                          <td style="text-align: end; ">
+                            
+                          <button style="border:none; background:none;" ><i style="margin-right: 15px;"><img src="icons/loupe.png" alt=""></i></button>
+                          <button style="border:none; background:none;" ><i style="margin-right: 15px;"><img src="icons/edit.png" alt=""></i></button>
+                          
+                          <form action="Liste_Attente_Resp.php" method="post" style="display:inline-block;">
+                            <input type="hidden" value="<?php echo $row['ID_OFFRE']; ?>" name="id_offre">
+                            <button type="submit" style="border:none; background:none;" ><i style="margin-right: 15px;"><img src="icons/file.png" alt=""></i></button>
+                          </form>
+                            
+                            
+                          </td>
+                        </tr>
+                      
+                        <?php endforeach; ?>
+  
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                          <th scope="col">N</th>
+                          <th scope="col">Statut</th>
+                          <th scope="col">Entreprise</th>
+                          <th scope="col">Ville</th>
+                          <th scope="col">Poste</th>
+                          <th scope="col"></th>
+                        </tr>
+                      </tfoot>
+                  </table>
               </div>
-
-            </div><br>
-            <?php $indice++; endforeach;} ?>            
           </div>
-          <div class="col-3 d-none d-md-block elm blank_col"></div>
-        </div>
-      </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
-    crossorigin="anonymous"></script>
-
-    <script>
-      
-      var scrollpos = localStorage.getItem('scrollpos_Find_Offre_Resp');
-      if (scrollpos){
-            window.scrollTo({left:0,top:scrollpos,behavior:'instant',});
-            localStorage.removeItem('scrollpos_Find_Offre_Resp');
-      }
-
-      function LastScroll(){
-        localStorage.setItem('scrollpos_Find_Offre_Resp', window.scrollY);
-      }
-      
-      function menuToggle(indice){
-        
-          const toggleMenu = document.getElementsByClassName("menu");
-          toggleMenu[indice].classList.toggle('active');
-        
           
-      };
 
-    </script>
+          
+          
 
+
+
+        </div>
+        </div>
+          
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+        crossorigin="anonymous">
+      
+      
+      </script>
+      <script>
+        function menuToggle(){
+            const toggleMenu = document.querySelector(".menu");
+            toggleMenu.classList.toggle('active');
+        }
+      </script>
+
+<script>
+  
+  $(document).ready( function () {
+    var dataTable = $('#Table_Offre').DataTable();
+
+
+
+    $('#Table_Offre tfoot tr th').each(function () {
+    var title = $('#Table_Offre thead tr th').eq($(this).index()).text();
+    if(title != "")
+    {
+      $(this).html('<input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1" placeholder="Search ' + title + '" />');
+    }
+    
+    });
+
+    dataTable.columns().every(function () {
+        var dataTableColumn = this;
+
+        $(this.footer()).find('input').on('keyup change', function () {
+            dataTableColumn.search(this.value).draw();
+        });
+    });
+    
+    }
+    )
+
+
+
+  
+
+</script>
+    
 </body>
 </html>
-
 <?php
   }
   else
